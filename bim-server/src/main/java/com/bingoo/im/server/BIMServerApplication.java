88e1f663cc2ee716c9eaa510c54bd5960b19eaaa
zhujianbin <zhujianbin>
2019-11-18 18:01:52 +0800

    create moduls

---
+++ bim-server/src/main/java/com/bingoo/im/server/BIMServerApplication.java
@@ -0,0 +1,18 @@
+package com.bingoo.im.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *  服务端启动类
+ * @author jianbin
+ * @date 2019/11/18
+ */
+public class BIMServerApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(BIMServerApplication.class);
+
+    public static void main(String[] args) {
+
+    }
+}
