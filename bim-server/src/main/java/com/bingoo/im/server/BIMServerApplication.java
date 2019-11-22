464ca4d79d5ecf3d4848d011d5f4cdd04d0eda6d
zhujianbin <zhujianbin>
2019-11-22 16:32:55 +0800

    即时通讯服务端

--- bim-server/src/main/java/com/bingoo/im/server/BIMServerApplication.java
+++ bim-server/src/main/java/com/bingoo/im/server/BIMServerApplication.java
@@ -1,5 +1,16 @@
 package com.bingoo.im.server;
 
+import com.bingoo.im.server.handle.WebSocketServerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,7 +23,25 @@ public class BIMServerApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(BIMServerApplication.class);
 
-    public static void main(String[] args) {
+    private static int port;
 
+    public static void main(String[] args) throws Exception {
+        new BIMServerApplication().run(port);
+    }
+
+    private void run(int port) throws Exception {
+        NioEventLoopGroup workGroup = new NioEventLoopGroup();
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new WebSocketServerInitializer());
+            Channel channel = bootstrap.bind(port).sync().channel();
+            channel.closeFuture().sync();
+        } finally {
+            workGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
     }
 }
---
+++ bim-server/src/main/java/com/bingoo/im/server/handle/WebSocketServerInitializer.java
@@ -0,0 +1,16 @@
+package com.bingoo.im.server.handle;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+
+/**
+ * @author jianbin
+ * @date 2019/11/19
+ */
+public class WebSocketServerInitializer extends ChannelInitializer<Channel> {
+
+    @Override
+    protected void initChannel(Channel channel) throws Exception {
+
+    }
+}
